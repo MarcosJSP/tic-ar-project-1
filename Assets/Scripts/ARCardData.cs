972972cc87d8fb173ff0d07d9aff4126d67eb7b1
marcosjsp <marcosjsp.ia@gmail.com>
2025-11-02 12:55:17 +0000

    Add Whale Shark textures and update scene and scripts
    - Added new textures for Whale Shark: base color, metallic roughness, and normal map.
    - Updated ReferenceImageLibrary to include WhaleSharkMarker.
    - Modified SampleScene to include a new tracked image reference.
    - Enhanced ARCardData script to allow for a multi-line title.
    - Updated simulation environment prefab with new tracked image components.

--- Assets/Scripts/ARCardData.cs
+++ Assets/Scripts/ARCardData.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ARCardData : MonoBehaviour
 {
     [Header("Card Information")]
+    [TextArea(1, 2)]
     public string title;
     [TextArea(3, 5)]
     public string description;
